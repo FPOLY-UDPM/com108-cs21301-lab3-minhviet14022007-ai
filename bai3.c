1ebe479c7f268e111371f2c03ae3ab472aa365c3
Trương Minh Việt <minhviet14022007@gmail.com>
2026-01-18 23:25:42 +0700

    TruongMinhViet_COM108_CS21301_Lab3

--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Trương Minh Việt]
+ * MSSV:      [PS48885]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TIỀN ĐIỆN 
@@ -11,13 +11,25 @@
 #include <stdio.h>
 
 int main(){
-    
+    float SoDien, TienDien;
     // Khai báo biến
-
-
+    printf("Nhap so dien tieu thu trong thang: ");
+    scanf("%f", &SoDien);
     // Nhập dữ liệu
-
-
+    if (SoDien < 0) {
+        printf("So dien tieu thu khong hop le! Vui long nhap so duong.\n");
+    } 
+    else if (SoDien <= 50) {
+        TienDien = SoDien * 1000;
+        printf("So tien dien phai dong: %.2f VND\n", TienDien);
+    } 
+    else {
+        TienDien = 50 * 1000 + (SoDien - 50) * 1200;
+        printf("So tien dien phai dong: %.2f VND\n", TienDien);
+    }
+    if (SoDien >= 0){
+    printf("So tien dien phai dong: %.2f VND\n", TienDien);
+    }
     // Xử lý, tính toán VÀ Hiển thị kết quả
-
+return 0;
 }
